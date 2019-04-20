3e7aaf7d75f7e7d3d7fdf2f1e1a3e82f8201b84b
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2019-04-20 15:01:57 +0300

    Embedding dropout (disabled by defaullt)

--- jigsaw/models.py
+++ jigsaw/models.py
@@ -14,6 +14,7 @@ class SimpleLSTM(nn.Module):
             ):
         super().__init__()
         self.embedding = nn.Embedding(n_vocab, n_embed)
+        # self.embedding_dropout = SpatialDropout(0.2)
         self.lstm = nn.LSTM(n_embed, n_lstm,
                             bidirectional=True, batch_first=True)
         self.linear_1 = nn.Linear(2 * n_lstm, n_linear)
@@ -23,6 +24,7 @@ class SimpleLSTM(nn.Module):
 
     def forward(self, x, lengths):
         x = self.embedding(x)
+        # x = self.embedding_dropout(x)
         x = pack_padded_sequence(x, lengths, batch_first=True)
         x, _ = self.lstm(x)
         x, _ = pad_packed_sequence(x, batch_first=True)
@@ -38,3 +40,13 @@ class SimpleLSTM(nn.Module):
 def TinyLSTM(n_vocab: int, n_embed: int):
     return SimpleLSTM(n_vocab=n_vocab, n_embed=n_embed,
                       n_lstm=32, n_linear=32)
+
+
+class SpatialDropout(nn.Dropout2d):
+    def forward(self, x):
+        x = x.unsqueeze(2)    # (N, T, 1, K)
+        x = x.permute(0, 3, 2, 1)  # (N, K, 1, T)
+        x = super(SpatialDropout, self).forward(x)  # (N, K, 1, T)
+        x = x.permute(0, 3, 2, 1)  # (N, T, 1, K)
+        x = x.squeeze(2)  # (N, T, K)
+        return x
