b530ac116de3829830d8f07bd57c0eee42c6827b
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2019-06-18 12:21:10 +0300

    New folds

--- jigsaw/folds.py
+++ jigsaw/folds.py
@@ -1,30 +1,30 @@
-import argparse
 import json
 from pathlib import Path
 
-from iterstrat.ml_stratifiers import MultilabelStratifiedKFold
 import pandas as pd
 
 from .metrics import IDENTITY_COLUMNS
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--n-folds', type=int, default=10)
-    args = parser.parse_args()
-
-    df = pd.read_csv('data/train.csv')
-    df = pd.concat([
-        df[df['identity_annotator_count'] > 0],
-        df[df['identity_annotator_count'] == 0].sample(frac=0.29),
-    ]).sample(frac=1)
-    print('Identity labeled', (df['identity_annotator_count'] > 0).mean())
-    ys = df[IDENTITY_COLUMNS + ['target']].fillna(0).values
-    ys = ys > 0.5
-    kfold = MultilabelStratifiedKFold(
-        n_splits=args.n_folds, shuffle=True, random_state=42)
-    folds = [list(map(int, df.iloc[valid_ids]['id'].values))
-             for _, valid_ids in kfold.split(df, ys)]
+    df = pd.read_pickle('data/train.pkl')
+    annot_df = (df[df['identity_annotator_count'] > 0]
+                .sample(n=48660, random_state=13))
+    not_annot_df = (df[df['identity_annotator_count'] == 0]
+                    .sample(n=48660, random_state=13))
+    valid_df = pd.concat([annot_df, not_annot_df])
+    train_df = df[~df['id'].isin(valid_df['id'])]
+    assert len(valid_df) + len(train_df) == len(df)
+    train_df_check = pd.concat([
+        train_df[train_df['identity_annotator_count'].fillna(0) > 0],
+        train_df[train_df['identity_annotator_count'].fillna(0) == 0]
+        .sample(frac=0.27)
+    ])
+    for col in ['target', 'identity_annotator_count'] + IDENTITY_COLUMNS:
+        print(f'{col:<40} '
+              f'{(valid_df[col].fillna(0) >= 0.5).values.mean():.4f} '
+              f'{(train_df_check[col].fillna(0) >= 0.5).values.mean():.4f} ')
+    folds = [list(map(int, valid_df['id'].values))]
     print('fold sizes', list(map(len, folds)))
     Path('data/folds.json').write_text(json.dumps(folds, indent=4))
 
