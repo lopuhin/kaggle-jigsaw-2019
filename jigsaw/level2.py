6490c4b853089732e6a165b59434092bc1db57eb
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2019-06-21 10:00:33 +0300

    re-calibrate logits in the second level

--- jigsaw/level2.py
+++ jigsaw/level2.py
@@ -1,9 +1,10 @@
 import numpy as np
+from scipy.special import expit, logit
 
 from .metrics import compute_bias_metrics_for_model, IDENTITY_COLUMNS
 
 
-def improve_predictions(preds, iterative=False, min_gain=1e-4):
+def improve_predictions(preds, iterative=False, min_gain=1e-5):
     preds = preds.copy()
     base_auc = compute_bias_metrics_for_model(preds, 'prediction')['auc']
     results = []
@@ -11,9 +12,9 @@ def improve_predictions(preds, iterative=False, min_gain=1e-4):
         best_auc = base_auc
         best_delta = 0
         best_preds = preds
-        for delta in sorted(set([0] + list(np.linspace(-0.2, 0.2, 20)))):
+        for delta in sorted(set([0] + list(np.linspace(-0.4, 0.4, 40)))):
             hacked = preds.copy()
-            hacked.loc[hacked[subgroup] >= 0.5, 'prediction'] *= 1 + delta
+            _apply_delta(hacked, subgroup, delta)
             auc = compute_bias_metrics_for_model(hacked, 'prediction')['auc']
             if auc > best_auc:
                 best_delta = delta
@@ -28,5 +29,10 @@ def improve_predictions(preds, iterative=False, min_gain=1e-4):
     if not iterative:
         for subgroup, gain, delta in results:
             if gain >= min_gain:
-                preds.loc[preds[subgroup] >= 0.5, 'prediction'] *= 1 + delta
+                _apply_delta(preds, subgroup, delta)
     return preds
+
+
+def _apply_delta(df, subgroup, delta):
+    subgroup_probs = df[subgroup] >= 0.5, 'prediction'
+    df.loc[subgroup_probs] = expit(delta + logit(df.loc[subgroup_probs]))
